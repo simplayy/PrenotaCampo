99f649037ce3bfe64e040b6c206a9f42bd514ffe
Simone Nicolini <simnic2000@gmail.com>
2022-05-22 11:08:02 +0200

    aggiunte views home

--- gestore_campo/gestione/models.py
+++ gestore_campo/gestione/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Campo(models.Model):
+    indirizzo = models.CharField(max_length=200)
+    prezzo = models.FloatField(max_length=50)
+    mq = models.IntegerField(default=100)
--- gestore_campo/gestione/views.py
+++ gestore_campo/gestione/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def gestione_home(request):
+    return render(request,template_name="gestione/home.html")
